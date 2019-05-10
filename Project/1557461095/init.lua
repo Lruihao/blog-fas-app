450fa595eefd852ad8a71ee6596987195b20b6db
李瑞豪 <1074627678@qq.com>
2019-05-10 13:50:13 +0800

    Add files via upload

---
+++ Project/1557461095/init.lua
@@ -0,0 +1,13 @@
+packagename="com.lruihao.cn.client"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appcode="38"
+appname="博採眾長"
+template="bottom"
+appver="2.6.2"
---
+++ Project/1557461095/sub/1543626451/init.lua
@@ -0,0 +1,2 @@
+name="Games"
+template="tool"
